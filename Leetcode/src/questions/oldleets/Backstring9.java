3e51a67ed70025c41ce71e1c19371f65efa03903
codeWithPrak <prakhar.017@gmail.com>
2025-09-29 02:48:26 +0530

    Refactor Code

--- Leetcode/src/questions/oldleets/Backstring9.java
+++ Leetcode/src/questions/oldleets/Backstring9.java
@@ -7,7 +7,9 @@ public class Backstring9 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			
+
+
+		//
 		Scanner sc = new Scanner(System.in);
 		String one= sc.next();
 		String two= sc.next();
